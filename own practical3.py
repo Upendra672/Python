ae72092e33d1d8c1b8ea5061f8f0bfb8a003b608
upendrakumar Yadav <yadavupendar400@gmail.com>
2022-08-11 15:19:44 +0530

    Second program

---
+++ own practical3.py
@@ -0,0 +1,12 @@
+#PRATICAL3
+
+print("Enter the Purchase amount:")
+amount=int(input("Enter the Purchase amount:"))
+if amount <= 1000:
+    discount=int(1000*5)/100
+    print("Discount amount:",discount)
+elif amount >= 10000:
+    discount=int(1000*10)/100
+    print("Discount amount:",discount)
+else:
+    print("No discount for you")
